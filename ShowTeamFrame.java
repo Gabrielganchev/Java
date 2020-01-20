1a0836cd4a6796e8df5196a96a8b7c2e6b366227
Ganchev <gabriel_ganchev@abv.bg>
2020-01-20 12:31:48 +0200

    simple again

---
+++ ShowTeamFrame.java
@@ -0,0 +1,6 @@
+import java.io.IOException;
+class ShowTeamFrame{
+  public static void main(String args[]) throws IOException {
+    new TeamFrame();
+  }
+}
---
+++ TeamFrame.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+@SuppressWarnings("serial")
+public class TeamFrame extends JFrame{
+  public TeamFrame() throws IOException{
+      Player player;
+      Scanner keyboard = new Scanner (new File("boring.txt"));
+      for(int num = 1; num <=9;num++){
+        player = new Player(keyboard.nextLine(),keyboard.nextDouble());
+        keyboard.nextLine();
+        addPlayerinfo(player);
+
+      }
+      setTitle("The Hankees");
+      setLayout(new GridLayout(9,2,20,3));
+      setDefaultCloseOperation(EXIT_ON_CLOSE);
+      pack();
+      setVisible(true);
+    }
+    void addPlayerinfo(Player player){
+      add(new JLabel(" " + player.getName()));
+      add(new JLabel(player.getAverageString()));
+
+    }
+  }
